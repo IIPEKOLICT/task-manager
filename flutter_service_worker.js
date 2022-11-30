353e3d6ec97f51bc5562a9a78d78752c1fe57d2a
IIPEKOLICT <54858818+IIPEKOLICT@users.noreply.github.com>
2022-11-30 18:03:56 +0000

    Deploying to gh-pages from @ IIPEKOLICT/task-manager@83417e75c606f32a3fee7c131797ba3828ae28bf 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -15,15 +15,15 @@ const RESOURCES = {
 "canvaskit/profiling/canvaskit.wasm": "95a45378b69e77af5ed2bc72b2209b94",
 "canvaskit/canvaskit.js": "2bc454a691c631b07a9307ac4ca47797",
 "canvaskit/canvaskit.wasm": "bf50631470eb967688cca13ee181af62",
-"index.html": "13fb22ff968eaa5284f9a45a0f784d65",
-"/": "13fb22ff968eaa5284f9a45a0f784d65",
+"index.html": "c817fef86db147102653d4dff64ed331",
+"/": "c817fef86db147102653d4dff64ed331",
 "version.json": "2b521e10dfa0f067561de489a19d6620",
 "manifest.json": "906fc433ff2f17c2c8af10ca573ca807",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"main.dart.js": "08ae13b6907b800d9f28b1d25017afa7"
+"main.dart.js": "c37d5f0ec25b410fca41795b183ea28e"
 };
 
 // The application shell files that are downloaded before a service worker can
